0b8580df722dcc807f780e1846c8858c5d5da145
Michael Tien <mtien888@gmail.com>
2016-02-14 14:25:38 -0800

    Add current location.

--- app/src/main/java/com/ymsgsoft/michaeltien/hummingbird/MapsActivity.java
+++ app/src/main/java/com/ymsgsoft/michaeltien/hummingbird/MapsActivity.java
@@ -1,11 +1,20 @@
 package com.ymsgsoft.michaeltien.hummingbird;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,10 +23,24 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        ConnectionCallbacks, OnConnectionFailedListener {
     private final int MY_SEARCH_ACTIVITY_REQUEST_ID = 1;
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        //Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
     private GoogleMap mMap;
     private Button mSearchButton;
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+    protected int REQUEST_LOCATION = 101;
+    private Boolean locationReady = false, mapReady = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +49,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
         mSearchButton = (Button) findViewById(R.id.search_button);
         mSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +84,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -80,11 +122,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        rlp.setMargins(0, 0, 30, 30);
 
         // Add a marker in Sydney and move the camera
-        LatLng Vancouver = new LatLng( 49.264911, -123.241917 );
-        mMap.addMarker(new MarkerOptions().position(Vancouver).title("Marker in Vancouver"));
-        CameraPosition target = CameraPosition.builder().target(Vancouver).zoom(14).build();
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Vancouver));
+//        LatLng Vancouver = new LatLng( 49.264911, -123.241917 );
+//        mMap.addMarker(new MarkerOptions().position(Vancouver).title("Marker in Vancouver"));
+//        CameraPosition target = CameraPosition.builder().target(Vancouver).zoom(14).build();
+////        mMap.moveCamera(CameraUpdateFactory.newLatLng(Vancouver));
+//        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        if ( locationReady) {
+            showCurrentPosition();
+        } else {
+            mapReady = true;
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        } else {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                locationReady = true;
+                if ( mapReady) {
+                    showCurrentPosition();
+                    mapReady = false;
+                }
+//            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+//            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            }
+        }
+
+    }
+    protected void showCurrentPosition()
+    {
+        LatLng Here = new LatLng( mLastLocation.getLatitude(), mLastLocation.getLongitude() );
+        mMap.addMarker(new MarkerOptions().position(Here).title("You Are Here"));
+        CameraPosition target = CameraPosition.builder().target(Here).zoom(14).build();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 }
