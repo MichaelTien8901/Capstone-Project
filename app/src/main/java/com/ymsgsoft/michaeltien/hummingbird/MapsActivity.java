049cf32df34ee5857c6f93e3dd4d418e500949c4
Michael Tien <mtien888@gmail.com>
2016-03-24 15:07:58 -0700

    Clear up (delete TestActivity).

--- app/src/main/java/com/ymsgsoft/michaeltien/hummingbird/MapsActivity.java
+++ app/src/main/java/com/ymsgsoft/michaeltien/hummingbird/MapsActivity.java
@@ -216,7 +216,7 @@ public class MapsActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.test, menu);
+        getMenuInflater().inflate(R.menu.menu_main_options, menu);
         return true;
     }
     @Override
